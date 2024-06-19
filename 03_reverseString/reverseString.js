bb2120307851614a4cd45a96e1594cf01933941f
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 15:21:51 -0700

    Convert reverse string pseudocode to code and past 1st test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,13 @@
 const reverseString = function(string) {
     // Initialize empty string and store as variable
+    let reversedString = '';
     // loop through length of given string, starting from end, ending at start
-    // in loop, add the current character to initialized string
+    for (let charPosition = string.length - 1; charPosition >= 0; charPosition--) {
+        // in loop, add the current character to initialized string
+        reversedString += string.charAt(charPosition);
+    }
     // return the new reversed string
+    return reversedString;
 };
 
 // Do not edit below this line
