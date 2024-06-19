4dfddaa59052e42d94bab0bfcd43dd42230dcdf9
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 15:17:40 -0700

    Add pseudocode for reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Initialize empty string and store as variable
+    // loop through length of given string, starting from end, ending at start
+    // in loop, add the current character to initialized string
+    // return the new reversed string
 };
 
 // Do not edit below this line
