c5d186b0a50a1469b53b8c23464f07a97afd3023
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 15:03:11 -0700

    Add pseudocode for repeated string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfIterations) {
+    // Initialize empty string that will store the repeated string, and store in variable
+    // Make loop based on number of iterations to add to string x times
+    // in the loop, add the given string to the new string
+    // return new string that contains the repeated string based on number of iterations
 };
 
 // Do not edit below this line
