ac6b2fd7a7847ed6f04ae6504ead1633e25572df
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 19:34:19 -0700

    Add leap year pseudocode

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // if year divided by 4 has no remainder, true that the year is leap year
+    // if year divided by 100 has no remainder, check further
+    // if year divided by 400 has no remainder, true that the year is leap year
+    // otherwise, year is not leap year
+    // otherwise, year is not leap year
 };
 
 // Do not edit below this line
