3d903519f7bcee1f697f2638369dedb76a028ae7
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 17:41:04 -0700

    Convert remove from array pseudocode to code and pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,14 @@
 const removeFromArray = function(array, ...elementsToRemove) {
     // loop through elements to remove 
-    // while the specific element is still in the given array
-    // remove it
+    for (const element of elementsToRemove) {
+        // while the specific element is still in the given array
+        while (array.indexOf(element) !== -1) {
+            // remove it
+            array.splice(array.indexOf(element), 1);
+        }
+    }
     // return new array with specified elements removed
+    return array;
 };
 
 // Do not edit below this line
