a41b745aec206baf723d92ab21e13b95998b63b5
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 19:54:31 -0700

    Convert temperature conversion pseudocode to code and pass first test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
 const convertToCelsius = function(fahrenheit) {
   // use equation to convert given fahrenheit to celcius and store in variable
+  let celcius = (fahrenheit - 32) * (5 / 9);
   // round to tenths place
+  return Math.round(celcius * 10) / 10;
 };
 
 const convertToFahrenheit = function(celcius) {
   // use equation to convert given celcius to fahrenheit and store in variable
+  let fahrenheit = celcius * (9 / 5) + 32;
   // round to tenths place
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
