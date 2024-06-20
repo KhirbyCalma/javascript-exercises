a94d759bd5f2f75499e6d84d8dd2b855c701afb6
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 19:39:53 -0700

    Swapped first two guard clauses for century year test case

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,6 @@
 const leapYears = function(year) {
-    // if year divided by 4 has no remainder, true that the year is leap year
-    if (year % 4 === 0) {
-        return true;
-    }
     // if year divided by 100 has no remainder, check further
-    else if (year % 100 === 0) {
+    if (year % 100 === 0) {
         // if year divided by 400 has no remainder, true that the year is leap year
         if (year % 400 === 0) {
             return true;
@@ -13,6 +9,10 @@ const leapYears = function(year) {
         else {
             return false;
         }
+    }
+    // if year divided by 4 has no remainder, true that the year is leap year
+    else if (year % 4 === 0) {
+        return true;
     }
     // otherwise, year is not leap year
     else {
