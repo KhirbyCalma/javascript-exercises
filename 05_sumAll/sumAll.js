fbc6dd87325a6d15d6b6f04f0648d2b92ca55238
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 17:49:39 -0700

    Add pseudocode for sum all exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    // initialize number to 0 and store as variable
+    // loop starting at start number and ending on ending number (inclusively)
+    // add to initialized number based on number current at in the loop
+    // return initialized number
 };
 
 // Do not edit below this line
