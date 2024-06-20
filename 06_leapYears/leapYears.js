58a478d37cae25c1e3fdae82a43ce26c9ca16b0a
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 19:36:20 -0700

    Convert leap year pseudocode to code and pass first test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,23 @@
 const leapYears = function(year) {
     // if year divided by 4 has no remainder, true that the year is leap year
+    if (year % 4 === 0) {
+        return true;
+    }
     // if year divided by 100 has no remainder, check further
-    // if year divided by 400 has no remainder, true that the year is leap year
+    else if (year % 100 === 0) {
+        // if year divided by 400 has no remainder, true that the year is leap year
+        if (year % 400 === 0) {
+            return true;
+        }
+        // otherwise, year is not leap year
+        else {
+            return false;
+        }
+    }
     // otherwise, year is not leap year
-    // otherwise, year is not leap year
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
