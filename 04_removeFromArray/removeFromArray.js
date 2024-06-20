a2beae119531ee613812208d744ebb6241800376
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-19 17:28:40 -0700

    Add pseudocode for remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    // loop through elements to remove 
+    // while the specific element is still in the given array
+    // remove it
+    // return new array with specified elements removed
 };
 
 // Do not edit below this line
