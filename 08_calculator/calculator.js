af672de1b0ac8e5576515292fc889ff5bfca9df8
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-28 13:27:11 -0700

    Implement factorial function and test successfully in calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,12 @@ const power = function(base, power) {
 	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  // base case
+	if (num <= 0) {
+    return 1;
+  } 
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
