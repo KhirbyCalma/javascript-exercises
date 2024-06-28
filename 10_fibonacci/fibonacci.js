659dcff6c3cb4b91db4c4c46c2780a44054a63a4
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-28 14:17:15 -0700

    Implement fibonacci function and test successfully in fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {   
+    if (num < 0) {
+        return 'OOPS';
+    }
+    if (parseInt(num) === 0 || parseInt(num) === 1) {
+        return parseInt(num);
+    }  
+    return fibonacci(parseInt(num) - 1) + fibonacci(parseInt(num) - 2);
 };
 
 // Do not edit below this line
