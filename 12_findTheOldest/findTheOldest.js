d64999c05e5079636a3658ce09cca0b593bc43fd
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-28 14:42:09 -0700

    Implement find oldest person function and test successfully in find oldest person exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge(person) {
+    return ("yearOfDeath" in person) ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    let oldestPerson = people.reduce((oldestPerson, person) => {
+        if (getAge(person) > getAge(oldestPerson)) {
+            return person;
+        } 
+        return oldestPerson;
+    }, people[0]);
+    return oldestPerson;
 };
 
 // Do not edit below this line
