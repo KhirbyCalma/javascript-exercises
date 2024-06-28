2a6111315e6bf2b8d85e7668dbb2ae20992275bc
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-28 14:08:24 -0700

    Implement palindrome function and test successfully in palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,33 @@
-const palindromes = function () {
+function checkCharacterIsAlphaNumeric(char) {
+    // refer to ascii table
+    let charCode = char.charCodeAt(0);
+    if ((charCode >= 48 && charCode <= 57) || // numeric (0-9)
+        (charCode >= 97 && charCode <= 122)) { // lower alpha (a-z)
+      return true;
+    }
+    return false;
+}
 
+const palindromes = function (string) {
+    let lowerCasedString = string.toLowerCase();
+    let leftPointer = 0;
+    let rightPointer = string.length - 1;
+    while (leftPointer <= rightPointer) {
+        // move left pointer until it hits valid alphanumeric
+        while (!checkCharacterIsAlphaNumeric(lowerCasedString.charAt(leftPointer))) {
+            ++leftPointer;
+        }
+        // move right pointer until it hits valid alphanumeric
+        while (!checkCharacterIsAlphaNumeric(lowerCasedString.charAt(rightPointer))) {
+            --rightPointer;
+        }
+        if (lowerCasedString.charAt(leftPointer) !== lowerCasedString.charAt(rightPointer)) {
+            return false;
+        }
+        ++leftPointer;
+        --rightPointer;
+    }
+    return true;
 };
 
 // Do not edit below this line
